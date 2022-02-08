e4839c6975a9f3f53f057bf9663db2b969b9002f
Jeff Destine <jd@jds-macbook-pro.lan>
2022-02-07 21:01:32 -0500

    update

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ func hello(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/"{
 		http.NotFound(w, r)
     }
-	  fmt.Fprintf(w, `al konyen bouzin, Jeff genyen yo gwo zozo`)
+	  fmt.Fprintf(w, `Jeff and Gilbert are learning how to build and deploy applications")
 	  return
   }
 
